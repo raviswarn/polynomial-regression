854de53ea96266f9e25f2ac4febd2107f6fc2092
Raveendra Swarna <swarnaravistar@gmail.com>
2017-12-18 16:35:45 -0600

    added data and ggplot

--- polynomialregression.py
+++ polynomialregression.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('data/Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
